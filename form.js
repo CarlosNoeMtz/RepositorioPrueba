f2a6c774d6c11b74bf35f6351232010b500e13ab
CarlosNoeMtz <mrtnzcarlosnoe@gmail.com>
2024-03-17 17:10:54 -0600

    Update Pagina de Contacto

--- form.js
+++ form.js
@@ -2,8 +2,6 @@ const nombre = document.getElementById("myname");
 const apellidos = document.getElementById("surname");
 const correo = document.getElementById("email");
 const celular = document.getElementById("mobile");
-const contrasenia = document.getElementById("password");
-const contrasenia2 = document.getElementById("repeatPassword");
 const terminosYcondiciones = document.getElementById("termsAndConditions");
 const form = document.getElementById("form");
 const listInputs = document.querySelectorAll(".form-input");
@@ -24,35 +22,27 @@ function validacionForm() {
   });
 
   if (nombre.value.length < 1 || nombre.value.trim() == "") {
-    mostrarMensajeError("myname", "Nombre no valido*");
+    mostrarMensajeError("myname", "Campos Obligatorios*");
     condicion = false;
   }
   if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
-    mostrarMensajeError("surname", "Apellido no valido");
+    mostrarMensajeError("surname", "Campos obligatorios*");
     condicion = false;
   }
   if (correo.value.length < 1 || correo.value.trim() == "") {
-    mostrarMensajeError("email", "Correo no valido*");
+    mostrarMensajeError("email", "Campos obligatorios*");
     condicion = false;
   }
   if (
-    celular.value.length != 9 ||
+    celular.value.length != 10 ||
     celular.value.trim() == "" ||
     isNaN(celular.value)
   ) {
-    mostrarMensajeError("mobile", "Celular no valido*");
-    condicion = false;
-  }
-  if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
-    mostrarMensajeError("password", "Contraseña no valido*");
-    condicion = false;
-  }
-  if (contrasenia2.value != contrasenia.value) {
-    mostrarMensajeError("repeatPassword", "Contraseña error*");
+    mostrarMensajeError("mobile", "Campos obligatorios*");
     condicion = false;
   }
   if (!terminosYcondiciones.checked) {
-    mostrarMensajeError("termsAndConditions", "Acepte*");
+    mostrarMensajeError("termsAndConditions", "Acepte los Terminos y Condiciones");
     condicion = false;
   } else {
     mostrarMensajeError("termsAndConditions", "");
@@ -67,5 +57,5 @@ function mostrarMensajeError(claseInput, mensaje) {
 
 function enviarFormulario() {
   form.reset();
-  form.lastElementChild.innerHTML = "Listo !!";
+  form.lastElementChild.innerHTML = "Listo, Pronto te Contactaremos";
 }
